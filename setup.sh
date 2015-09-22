ad8017bf139d08132a0c5e9b6be73c9f1bba03b8
Jonas Hagstedt <hagstedt@gmail.com>
2015-09-22 10:59:32 +0200

    fix setup

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 cd javascript && npm install
+cd ../demo/static/js && npm install
 cd ../demo && ./manage.py migrate
-cd ../demo && ./manage.py makedata
 cd ../demo && ./manage.py runserver
