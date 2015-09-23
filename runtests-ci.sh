64c40fbdd86bdd3cdbc89ed3fd4cc044561cd7ce
Jonas Hagstedt <hagstedt@gmail.com>
2015-09-23 13:04:10 +0200

    meh

--- runtests-ci.sh
+++ runtests-ci.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
+
 # Build js
 cd ./javascript
 npm run build
 cd ..
 
 # Start the template server
-node ./javascript/dist/template-server.js socketPath=./template-server.sock &
+#node ./javascript/dist/template-server.js socketPath=./template-server.sock &
 
 ./runtests.sh
