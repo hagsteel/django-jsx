803dbdea4e2c44f844e75a81833bd0b3d66a90ea
Jonas Hagstedt <hagstedt@gmail.com>
2015-09-23 13:55:24 +0200

    comeon

--- tests/test_x.py
+++
@@ -1,8 +0,0 @@
-from django.conf import settings
-from django.test import TestCase
-from django_jsx.template.backend import JsTemplates, JsTemplate
-
-
-class TestX(TestCase):
-    def testx(self):
-        print(settings.DJANGO_ISOMORPHIC_SOCKET_PATH)
