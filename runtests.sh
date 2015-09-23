384ae8535eb35c13a029a4048228ac0ab584e4a2
Jonas Hagstedt <hagstedt@gmail.com>
2015-09-23 13:57:49 +0200

    comeon

--- runtests.sh
+++ runtests.sh
@@ -4,5 +4,4 @@
 babel ./tests/template_src --out-dir ./tests/templates/
 
 # Run the tests
-#py.test -s --cov-report term-missing --cov=django_jsx $1
-py.test -s $1
+py.test -s --cov-report term-missing --cov=django_jsx $1
