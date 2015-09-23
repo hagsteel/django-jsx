0d78ee5e9f98d7cc6ee3bbb97445575ed83dce17
Jonas Hagstedt <hagstedt@gmail.com>
2015-09-23 11:52:24 +0200

    removed stupid

--- runtests-ci.sh
+++ runtests-ci.sh
@@ -4,12 +4,6 @@ cd ./javascript
 npm run build
 cd ..
 
-# Install react
-cd ./tests/template_src/
-npm install react
-cd ..
-cd ..
-
 # Start the template server
 node ./javascript/dist/template-server.js socketPath=./template-server.sock &
 
